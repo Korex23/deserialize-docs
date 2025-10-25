e3b52e074b07d05e6978cf56e6625d39e4b40045
Korex23 <ololokes@gmail.com>
2025-10-25 10:21:53 +0100

    fix

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -6,11 +6,11 @@ export default function Home() {
   return (
     <main className="min-h-[calc(100vh-36px)] bg-white text-gray-900">
       <section className="max-w-5xl mx-auto px-6 py-16 text-center">
-        <h1 className="text-5xl md:text-7xl font-extrabold tracking-tight text-emerald-600">
+        <h1 className="text-3xl md:text-7xl font-extrabold tracking-tight text-emerald-600">
           Deserialize Documentation
         </h1>
 
-        <p className="mt-6 text-gray-700 text-lg max-w-2xl mx-auto">
+        <p className="mt-6 text-gray-700 text-sm md:text-lg max-w-2xl mx-auto">
           Robust SDKs and REST APIs for secure swaps and intelligent token
           screening on Eclipse. Designed for performance, reliability, and ease
           of integration.
